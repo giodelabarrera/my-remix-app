331cf861c3e69f26b4410e4d5785137159af03ca
Giorgio de la Barrera <giodelabarrera@gmail.com>
2023-10-13 11:22:15 +0200

    Set initial-scale to 1

--- app/root.tsx
+++ app/root.tsx
@@ -60,7 +60,7 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=10" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
